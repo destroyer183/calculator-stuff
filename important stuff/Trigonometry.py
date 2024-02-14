fa591701faf828cbdccb16eb237dce19e934a5e3
destroyer183 <jzante99@gmail.com>
2024-02-14 14:17:05 -0500

    started adding comments for trig calculator

--- important stuff/Trigonometry.py
+++ important stuff/Trigonometry.py
@@ -7,6 +7,8 @@ import enum
 
 ''' NOTES
 
+if the user inputs 4 values, like 3 sides and 1 angle, and the triangle wouldn't technically be possible, make code to check for this.
+
 add thingy that points to the 'clear' button if someone keeps trying to change values after they were calculated
 
 allow the user to input angles over 180* by using modulus
@@ -14,14 +16,17 @@ allow the user to input angles over 180* by using modulus
 '''
 
 
+# the max pixel side length for the triangle displayed
 MAX_SIDE_LENGTH = 450
 
+# reference x positions to make it easier to move boxes around when the mode is changed
 ANGLE_TEXT_REFX = 100
 ANGLE_BOX_REFX  = 170
 
 LENGTH_TEXT_REFX = 350
 LENGTH_BOX_REFX = 420
 
+# enums
 class Trig(enum.Enum):
     SIDE_ANGLE_SIDE = 'side angle side'
     SIDE_SIDE_SIDE = 'side side side'
@@ -59,6 +64,7 @@ def get_super(x):
 
 
 
+# find item in a container
 def find(container, value):
 
     for index, element in enumerate(container):
@@ -66,6 +72,8 @@ def find(container, value):
         if element == value: return index
 
 
+
+# find an item in a container but loop backwards
 def rfind(container, value):
 
     for index in range(len(container) - 1, -1, -1):
@@ -74,14 +82,16 @@ def rfind(container, value):
 
 
 
+# class for the logic of the trig calculations
 class Logic:
 
     def __init__(self, is_ambiguous = False, name = '') -> None:
 
+        # variable to store whether or not a triangle is ambiguous, 
+        # there will be two instances of this class, one that is for the ambiguous triangle, the other for the normal triangle.
         self.is_ambiguous = is_ambiguous
 
-        self.name = name
-
+        # data for the triangle
         self.angles  = [60, 60, 60]
         self.lengths = [1, 1, 1]
         self.coordinates   = {"a": [0, 0], "b": [0, 0], "c": [0, 0]}
@@ -90,36 +100,53 @@ class Logic:
 
 
 
+    # calculations for cosine law
     def cos_law(self, type, index):
 
+        # check if the side-angle-side cosine law is being used
         if type == Trig.SIDE_ANGLE_SIDE:
             
+            # sum the squares of the two given sides
             part_1 = self.info(Info.LEFT_SIDE, index) ** 2 + self.info(Info.RIGHT_SIDE, index) ** 2
+
+            # multiply the two given sides together and then multiply by two and then multiply by the cosine of the given angle
             part_2 = 2 * self.info(Info.LEFT_SIDE, index) * self.info(Info.RIGHT_SIDE, index) * math.cos(math.radians(self.info(Info.OPPOSITE_ANGLE, index)))
 
+            # return the square root of part 1 minus part 2
             return math.sqrt(part_1 - part_2)
 
+        # check if the side-side-side cosine law is being used
         if type == Trig.SIDE_SIDE_SIDE:
 
+            # calculate the numerator of the equation, left side squared plus right side squared minus opposite side squared
             numerator = self.info(Info.LEFT_SIDE, index) ** 2 + self.info(Info.RIGHT_SIDE, index) ** 2 - self.lengths[index] ** 2
+
+            # calculate the denominator of the equation, two times the left side times the right side
             denominator = 2 * self.info(Info.LEFT_SIDE, index) * self.info(Info.RIGHT_SIDE, index)
 
+            # return the inverse sine of the numerator divided by the denominator
             return math.degrees(math.acos(numerator / denominator))
         
 
-
+ 
+    # function to calculate sine law
     def sin_law(self, type, index, angle_index):
 
+        # check if an angle is being calculated
         if type == Trig.ANGLE:
 
+            # return the inverse sine of the sine of the given angle divided by the opposite side, divided by the opposite side of the angle we are solving for.
             return math.degrees(math.asin(math.sin(math.radians(self.angles[angle_index])) / self.info(Info.OPPOSITE_SIDE, angle_index) * self.info(Info.OPPOSITE_SIDE, index)))
         
+        # check if a side is being solved
         elif type == Trig.SIDE:
 
+            # return the sine of the opposite angle divided by (the sine of the given angle divided by the opposite side)
             return math.sin(math.radians(self.info(Info.OPPOSITE_ANGLE, index))) / (math.sin(math.radians(self.angles[angle_index])) / self.info(Info.OPPOSITE_SIDE, angle_index))
         
 
 
+    # function to return information about the current triangle
     def info(self, request, index, return_type = 0):
 
         if request == Info.OPPOSITE_SIDE:
@@ -168,15 +195,18 @@ class Logic:
             if index == 1: array, i = self.angles, 0
             if index == 2: array, i = self.angles, 1
 
+        # return just the index of the requested information
         if return_type: return i
 
+        # return the requested information
         return array[i]
 
 
 
+    # function to check if the triangle is solvable
     def check_triangle(self):
 
-        # calculate if triangle is solvable
+        # count the amount of given information
         count = 0
         for index in range(len(self.angles)):
             if self.angles[index] != 0: count += 1
@@ -184,54 +214,73 @@ class Logic:
         for index in range(len(self.lengths)):
             if self.lengths[index] != 0: count += 1
 
+        # if the count is zero, return an identifier that tells the calculator to clear the current data
         if not count: return Data.CLEAR_DATA, 0
 
+        # if the count is less than three, return an identifier that tells the calculator that there isnt enough information to solve the triangle
         if count < 3: return Data.UNSOLVABLE, 0
 
+        # if there aren't any given sides, automatically fill in values for the sides.
         if max(self.lengths) == 0 and count <= 3: 
             self.lengths = [1, 1, 1]
 
 
 
-        # DON'T FORGET ABOUT THE AMBIGUOUS CASE!!!
+        # loop over the angles to check for ambiguous case
         for index in range(len(self.angles)):
 
+            # check if the current angle is less than 90 degrees, and
+            # the adjacent left side is longer than the opposite side, and the opposite side isn't zero, or
+            # the adjacent right side is longer than the opposite side, and the opposite side isn't zero
             if 0 < self.angles[index] < 90 and (
                     (self.info(Info.ADJACENT_SIDE_LEFT, index)  > self.info(Info.OPPOSITE_SIDE, index) and self.info(Info.OPPOSITE_SIDE, index)) or
                     (self.info(Info.ADJACENT_SIDE_RIGHT, index) > self.info(Info.OPPOSITE_SIDE, index) and self.info(Info.OPPOSITE_SIDE, index))):
 
-                # do something for ambiguous case
+                # return an identifier that tells the calculator that the inputted data is ambiguous
                 return 'yes', True
             
-        # check if triangle can exist
+        # if there are 3 given side lengths, check if the side lengths won't display a triangle with connected edges
         if min(self.lengths):
-            if max(self.lengths) >= 2 * (self.lengths[0] + self.lengths[1] + self.lengths[2] - max(self.lengths)): return Data.IMPOSSIBLE, 0
+            if max(self.lengths) >= (self.lengths[0] + self.lengths[1] + self.lengths[2] - max(self.lengths)): return Data.IMPOSSIBLE, 0
 
+        # if nothing else triggers, return identifier that tells the calculator the triangle is solvable.
         return 'yes', False
 
 
 
+    # function to solve the remaining values for the triangle
     def solve_triangle(self, ambiguous):
         
-        # all sides
+        # check if all sides are given
         if min(self.lengths):
+
+            # iterate through every angle value
             for index in range(len(self.angles)):
+
+                # if the angle value isn't given, solve the angle value.
                 if not self.angles[index]: self.angles[index] = self.cos_law(Trig.SIDE_SIDE_SIDE, index)
 
+            # return to avoid extra calculations that will create incorrect values
             return
         
-        # two sides one angle
+        # check if there is an ambiguous angle
         if ambiguous:
 
-            # find variable angle
+            # loop through every angle
             for index in range(len(self.lengths)):
 
+                # check if a given side has no given value for the opposite angle
                 if self.lengths[index] and not self.info(Info.OPPOSITE_ANGLE, index):
 
+                    # check if the current triangle is supposed to be the ambiguous case or not 
                     if self.is_ambiguous:
+
+                        # calculate obtuse angle for ambiguous triangle
                         self.angles[index] = 180 - self.sin_law(Trig.ANGLE, index, find(self.angles, max(self.angles)))
 
                     else:
+
+                        # calculate acute angle for ambiguous triangle
                         self.angles[index] = self.sin_law(Trig.ANGLE, index, find(self.angles, max(self.angles)))
 
 
